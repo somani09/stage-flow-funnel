7997663b1e5eda1243aa2c14e1aadf19f23fe044
Vaibhav Somani <vsomani2@gmail.com>
2025-07-20 13:17:19 -0500

    chore(sidebar): updated sidebar's positioning

--- app/page.tsx
+++ app/page.tsx
@@ -46,39 +46,43 @@ const Home = () => {
     : "300px";
 
   return (
-    <div className="flex h-max min-h-screen flex-col pt-12 pr-4 pb-6 pl-32 sm:pr-32">
+    <div className="flex h-max min-h-screen flex-col px-8 pt-12 sm:pl-32 md:pr-32">
       <h1 className="text-primary text-4xl font-bold lg:text-6xl">
         {pageConfig.title}
       </h1>
 
       <div className="text-secondary relative mt-6 text-sm sm:text-lg">
-        <div
-          className={cn(
-            "relative transition-[max-height] duration-500 ease-in-out sm:max-h-none sm:overflow-visible",
-            isExpanded ? "overflow-visible" : "overflow-hidden",
-          )}
-          style={{
-            maxHeight: isExpanded ? "none " : "2.5rem",
-          }}
-        >
-          <p>{pageConfig.description}</p>
-
-          {!isExpanded && (
-            <div className="pointer-events-none absolute bottom-0 left-0 h-8 w-full bg-gradient-to-t from-white to-transparent sm:hidden" />
-          )}
+        <div className="sm:hidden">
+          <div
+            className={cn(
+              "relative transition-[max-height] duration-500 ease-in-out",
+              isExpanded ? "overflow-visible" : "overflow-hidden",
+            )}
+            style={{
+              maxHeight: isExpanded ? "none" : "2.5rem",
+            }}
+          >
+            <p>{pageConfig.description}</p>
+            {!isExpanded && (
+              <div className="pointer-events-none absolute bottom-0 left-0 h-8 w-full bg-gradient-to-t from-white to-transparent" />
+            )}
+          </div>
+          <div className="mt-1 flex justify-end">
+            <button
+              className="text-primary text-xs font-semibold underline"
+              onClick={() => setIsExpanded((prev) => !prev)}
+            >
+              {isExpanded ? "Read less ↑" : "Read more ↓"}
+            </button>
+          </div>
         </div>
 
-        <div className="mt-1 flex justify-end sm:hidden">
-          <button
-            className="text-primary text-xs font-semibold underline"
-            onClick={() => setIsExpanded((prev) => !prev)}
-          >
-            {isExpanded ? "Read less ↑" : "Read more ↓"}
-          </button>
+        <div className="hidden sm:block">
+          <p>{pageConfig.description}</p>
         </div>
       </div>
 
-      <div className="bg-accent-1 mt-8 mb-8 h-0.5 max-w-48 rounded-full" />
+      <div className="bg-accent-1 mt-1 mb-8 h-0.5 max-w-48 rounded-full sm:my-8" />
 
       <div className="flex w-full flex-1 flex-col gap-6 lg:flex-row">
         <div
